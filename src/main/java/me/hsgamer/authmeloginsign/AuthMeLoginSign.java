aeb13afade9db8be1a4ef1d3fc9545e7635b2d1b
HSGamer <huynhqtienvtag@gmail.com>
2022-06-20 21:51:02 +0700

    to SignMenu

--- src/main/java/me/hsgamer/authmeloginsign/AuthMeLoginSign.java
+++ src/main/java/me/hsgamer/authmeloginsign/AuthMeLoginSign.java
@@ -3,9 +3,10 @@ package me.hsgamer.authmeloginsign;
 import fr.xephi.authme.api.v3.AuthMeApi;
 import me.hsgamer.authmeloginsign.config.MainConfig;
 import me.hsgamer.authmeloginsign.listener.PlayerListener;
-import me.hsgamer.authmeloginsign.signgui.SignGUIAPI;
+import me.hsgamer.authmeloginsign.signgui.SignMenuFactory;
 import me.hsgamer.hscore.bukkit.simpleplugin.SimplePlugin;
 import me.hsgamer.hscore.bukkit.utils.MessageUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -14,6 +15,7 @@ import java.util.List;
 public final class AuthMeLoginSign extends SimplePlugin {
     private final MainConfig mainConfig = new MainConfig(this);
     private AuthMeApi authMeApi;
+    private SignMenuFactory.Menu menu;
 
     @Override
     public void load() {
@@ -24,9 +26,10 @@ public final class AuthMeLoginSign extends SimplePlugin {
     public void enable() {
         authMeApi = AuthMeApi.getInstance();
         registerListener(new PlayerListener(this));
+        setupMenu();
     }
 
-    public void openGUI(Player player) {
+    private void setupMenu() {
         List<String> lines = MainConfig.LINES.getValue();
         lines.replaceAll(MessageUtils::colorize);
         int passwordLineIndex = 0;
@@ -38,25 +41,27 @@ public final class AuthMeLoginSign extends SimplePlugin {
                 break;
             }
         }
-
         int finalPasswordLineIndex = passwordLineIndex;
-        SignGUIAPI.builder()
-                .plugin(this)
-                .withLines(lines)
-                .action(event -> {
-                    String password = ChatColor.stripColor(event.getLines().get(finalPasswordLineIndex));
+
+        SignMenuFactory factory = new SignMenuFactory(this);
+        menu = factory.newMenu(lines)
+                .reopenIfFail(Boolean.TRUE.equals(MainConfig.FORCE_SIGN.getValue()))
+                .response((player, signLines) -> {
+                    String password = ChatColor.stripColor(signLines[finalPasswordLineIndex]);
                     if (authMeApi.isRegistered(player.getName())) {
                         if (authMeApi.checkPassword(player.getName(), password)) {
                             authMeApi.forceLogin(player);
-                        } else if (Boolean.TRUE.equals(MainConfig.FORCE_SIGN.getValue())){
-                            openGUI(player);
+                        } else {
+                            return false;
                         }
                     } else {
                         authMeApi.forceRegister(player, password, true);
                     }
-                })
-                .uuid(player.getUniqueId())
-                .build()
-                .open();
+                    return true;
+                });
+    }
+
+    public void openGUI(Player player) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> menu.open(player));
     }
 }
--- src/main/java/me/hsgamer/authmeloginsign/signgui/SignCompleteHandler.java
+++
@@ -1,6 +0,0 @@
-package me.hsgamer.authmeloginsign.signgui;
-
-@FunctionalInterface
-public interface SignCompleteHandler {
-    void onSignClose(SignCompletedEvent event);
-}
